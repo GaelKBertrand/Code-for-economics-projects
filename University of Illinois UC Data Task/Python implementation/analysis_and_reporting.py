f4b4867198cbf8a77c2cf4d3db986fc81ef0fcf6
BeeKay Koozie <kwibukabertrand@gmail.com>
2023-12-08 12:20:46 +0530

    Bug fixes

--- University of Illinois UC Data Task/Python implementation/analysis_and_reporting.py
+++ University of Illinois UC Data Task/Python implementation/analysis_and_reporting.py
@@ -1,106 +1,28 @@
-# Mount Google Drive
-from google.colab import drive
+#Merging and Data cleaning
+# Import necessary libraries for analysis
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-drive.mount('/content/drive')
+# Load generated datasets from Google Drive
+baseline_data = pd.read_csv('/content/drive/My Drive/baseline_survey_data.csv')
+random_assignment_data = pd.read_csv('/content/drive/My Drive/random_assignment_data.csv')
+endline_data = pd.read_csv('/content/drive/My Drive/endline_survey_data.csv')
 
-# Set random seed for reproducibility
-np.random.seed(42)
+# Merge datasets
+merged_data = pd.merge(baseline_data, random_assignment_data, on='Participant_ID')
+merged_data = pd.merge(merged_data, endline_data, on='Participant_ID')
 
+# Print column names in the merged dataset
+print("Column Names in Merged Dataset:", merged_data.columns)
 
-# Function to generate baseline survey data
-def generate_baseline_survey_data(num_participants):
-    data = {
-        'Participant_ID': list(range(1, num_participants + 1)),
-        'Age': np.random.randint(18, 65, size=num_participants),
-        'Gender': np.random.choice(['Male', 'Female'], size=num_participants),
-        'Location': np.random.choice(['Urban', 'Suburban', 'Rural'], size=num_participants),
-        'Education': np.random.choice(['High School', 'College', 'Graduate School'], size=num_participants),
-        'COVID_Awareness_Level': np.random.choice(['Low', 'Medium', 'High'], size=num_participants),
-        'Received_Info_Social_Media': np.random.choice(['Yes', 'No'], size=num_participants),
-        'Vaccine_Hesitancy': np.random.choice(['Yes', 'No', 'Not Sure'], size=num_participants),
-        'Reasons_For_Hesitancy': np.where(np.random.rand(num_participants) < 0.2, 'Concerns about side effects', ''),
-        # Add more baseline questions as needed
-    }
-    baseline_data = pd.DataFrame(data)
-
-    # Save baseline data to CSV on Google Drive
-    baseline_data.to_csv('/content/drive/My Drive/baseline_survey_data.csv', index=False, quoting=2)
-
-    return baseline_data
-
-
-# Generate baseline survey data
-baseline_data = generate_baseline_survey_data(5000)
-
-
-# Function to generate random assignment data
-def generate_random_assignment_data(num_participants):
-    data = {
-        'Participant_ID': list(range(1, num_participants + 1)),
-        'Ad_Type': np.random.choice(['Reason', 'Emotions', 'Control'], size=num_participants),
-        'Feel_About_Info_Social_Media': np.random.choice(['Positive', 'Neutral', 'Negative'], size=num_participants),
-    }
-    random_assignment_data = pd.DataFrame(data)
-
-    # Save random assignment data to CSV on Google Drive
-    random_assignment_data.to_csv('/content/drive/My Drive/random_assignment_data.csv', index=False, quoting=2)
-
-    return random_assignment_data
-
-
-# Generate random assignment data
-random_assignment_data = generate_random_assignment_data(5000)
-
-
-# Function to generate endline survey data
-def generate_endline_survey_data(baseline_data, num_endline_participants):
-    # Ensure the correct number of participants for endline survey
-    num_baseline_participants = len(baseline_data)
-    if num_endline_participants > num_baseline_participants:
-        raise ValueError(
-            f"num_endline_participants ({num_endline_participants}) should be less than or equal to the total number of baseline participants ({num_baseline_participants}).")
-
-    endline_participants = np.random.choice(baseline_data['Participant_ID'], size=num_endline_participants,
-                                            replace=False)
-
-    # Simulate changes in responses based on ad type
-    endline_data = baseline_data[baseline_data['Participant_ID'].isin(endline_participants)].copy()
-
-    # Add 'Ad_Type' column to endline_data
-    endline_data['Ad_Type'] = random_assignment_data['Ad_Type'].values[:num_endline_participants]
-
-    endline_data['Received_Vaccine'] = np.random.choice(['Yes', 'No'], size=len(endline_data))
-    endline_data['Influenced_Decision'] = np.where(endline_data['Received_Vaccine'] == 'Yes',
-                                                   np.random.choice(['Reason Ad', 'Emotions Ad', 'Other'],
-                                                                    size=len(endline_data)),
-                                                   'Not Applicable')
-
-    endline_data['Effectiveness_Reason_Ad'] = np.where(endline_data['Ad_Type'] == 'Reason',
-                                                       np.random.choice(['Not at all effective', 'Somewhat effective',
-                                                                         'Very effective'], size=len(endline_data)),
-                                                       '')
-
-    endline_data['Effectiveness_Emotions_Ad'] = np.where(endline_data['Ad_Type'] == 'Emotions',
-                                                         np.random.choice(['Not at all effective', 'Somewhat effective',
-                                                                           'Very effective'], size=len(endline_data)),
-                                                         '')
-    # Add more endline questions as needed
-
-    # Save endline data to CSV on Google Drive
-    endline_data.to_csv('/content/drive/My Drive/endline_survey_data.csv', index=False, quoting=2)
-
-    return endline_data
-
-
-# Generate endline survey data
-endline_data = generate_endline_survey_data(baseline_data, 4500)
-
-
-
+# Convert 'Received_Vaccine' to numeric
+merged_data['Received_Vaccine'] = pd.to_numeric(merged_data['Received_Vaccine'].replace({'Yes': 1, 'No': 0}))
+# Convert 'Vaccine_Hesitancy_x' to numeric values
+vaccine_hesitancy_mapping = {'No': 0, 'Yes': 1, 'Not Sure': 2}
+merged_data['Vaccine_Hesitancy_x'] = merged_data['Vaccine_Hesitancy_x'].map(vaccine_hesitancy_mapping)
 
 #Analysis and reporting figures and tables
 #Part 1: Descriptive Statistics and Visualizations:
-
 
 # Table: Count of participants in each group
 table1 = pd.crosstab(merged_data['Ad_Type_x'], merged_data['Received_Vaccine'], margins=True, margins_name="Total")
@@ -127,8 +49,9 @@ table3.columns = ["Did Not Receive Vaccine", "Received Vaccine", "Total"]
 # Calculate the effectiveness of the Emotions Ad by displaying the count of participants who received or did not receive the vaccine.
 display(table3)
 
-
 #Part 2: Statistical Tests and Modeling:
+
+## Part 2: Statistical Tests and Modeling:
 
 ## Part 2: Statistical Tests and Modeling:
 
@@ -245,7 +168,6 @@ else:
 # Present a practical significance context based on the p-value and effect size.
 
 
-
 #Part 3: Sub-group analysis
 
 
@@ -309,4 +231,3 @@ plt.xlabel('Ad Type')
 plt.ylabel('Proportion with Vaccine Hesitancy')
 plt.show()
 # Figure 5: Proportion of Participants with Vaccine Hesitancy by Ad Type
-
